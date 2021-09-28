704eae00269749956289e0a8c7c1e13d6079379a
shambarick <shambarick@users.noreply.github.com>
2021-09-28 08:09:02 -1000

    fix wsgi

--- app.py
+++ app.py
@@ -92,9 +92,5 @@ def healthcheck():
     })
 
 
-def main():
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
-
-
 if __name__ == '__main__':
-    main()
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,4 +3,4 @@ set -e
 
 PORT=${PORT:-5000}
 
-gunicorn --workers=5 --bind=0.0.0.0:${PORT} app:main
+gunicorn --workers=5 --bind=0.0.0.0:${PORT} wsgi:app
